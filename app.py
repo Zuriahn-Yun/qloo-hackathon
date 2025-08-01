946ad27146ce79c3e172f5fc6ffe3713e78433db
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2025-08-01 01:04:46 -0700

    Delete main

--- app.py
+++ app.py
@@ -131,5 +131,5 @@ def api_docs():
     return jsonify(docs)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='127.0.0.1', port=5000)
